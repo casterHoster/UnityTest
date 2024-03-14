9dccdfb77a19144dc5d641c72eec9397034f35ef
casterHoster <95349741+casterHoster@users.noreply.github.com>
2024-03-14 22:28:05 +0300

    commit

--- UnityTest/Assets/Scripts/Button.cs
+++ UnityTest/Assets/Scripts/Button.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Button : MonoBehaviour
+public class Button : MonoBehaviour, IPointerExitHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Color _color;
+
+    private Image _image;
+
+    private void Awake()
     {
-        
+        _image = GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeColor()
     {
-        
+        _image.color = _color;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _image.color = Color.white;
     }
 }
